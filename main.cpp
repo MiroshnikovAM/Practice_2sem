df77193531b86e02f0535dccf35e448ec174b404
MiroshnikovAM <kriogenika@yandex.ru>
2020-05-23 19:37:34 +0300

    code: stream in-out realization

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include "plant.h"
+#include <windows.h>
+#include <fstream>
 
 using namespace std;
 
@@ -13,46 +15,171 @@ struct INPUT_DATA {
     double** A;
 };
 
-void Fill_Data(INPUT_DATA & Data);
-void Dispose_Data(INPUT_DATA & Data);
+void FillData(INPUT_DATA & data);
+void DisposeData(INPUT_DATA & data);
+void DisposeVector(double** ptrVector);
+void DisposeMatrix(const INPUT_DATA & data, double*** ptrMatrix);
+void GetCurrentVector(const INPUT_DATA & data, Plant plant, double** ptrVector);
+void GetCalculatedVector(const INPUT_DATA & data, double* x, double** pZ);
+void WriteCalculatedVector(const INPUT_DATA & data, ofstream file, double* z);
+void ReadCalculatedVectors(const INPUT_DATA & data, ifstream file, double*** pZZ);
+double CalculateAverage(const INPUT_DATA & data, double* vector);
+double CalculateVariance(const INPUT_DATA & data, double average, double* vector);
+
 
 int main() {
-    INPUT_DATA Input_Data;
-    Fill_Data(Input_Data);
+    INPUT_DATA params;
+    FillData(params);
+    Plant plant;
+    plant_init(plant);
+
+    fstream file;
+    file.open("data.raw", ios::binary | ios::out | ios::trunc | ios::in);
+
+    double* x = 0;
+    double* z = 0;
+
+    for (size_t i = 0; i < params.N; i++) {
+        GetCurrentVector(params, plant, &x);
+        GetCalculatedVector(params, x, &z);
+        WriteCalculatedVector(params, (ofstream)file, z);
+        Sleep(params.T);
+    }
+    DisposeVector(&x);
+    DisposeVector(&z);
+
+    double** zz = 0;
+
+    ReadCalculatedVectors(params, (ifstream)file, &zz);
+    double average;
+    double variance;
+    for (size_t i = 0; i < params.l; i++) {
+        average = CalculateAverage(params, zz[i]);
+        variance = CalculateVariance(params, average, zz[i]);
+        cout << "avg[" << i << "] = " << average << endl;
+        cout << "var[" << i << "] = " << variance << endl;
+    }
+    DisposeMatrix(params, &zz);
+
+    DisposeData(params);
     return 0;
 }
 
-void Fill_Data(INPUT_DATA & Data) {
-    Data.k = 4;
-    Data.M = new size_t [Data.k];
-    for (size_t i = 0; i < Data.k; i++) {
-        Data.M[i] = i;
+
+void FillData(INPUT_DATA & data) {
+    data.k = 4;
+    data.M = new size_t [data.k];
+    for (size_t i = 0; i < data.k; i++) {
+        data.M[i] = i + 1;
     }
-    Data.T = 10;
-    Data.N = 5;
-    Data.l = 2;
-    Data.A = new double* [Data.l];
-    for (size_t i = 0; i < Data.l; i++) {
-        Data.A[i] = new double [Data.k+1];
-        for (size_t j = 0; j < Data.k+1; j++) {
-            Data.A[i][j] = 1;
+    data.T = 2000;
+    data.N = 5;
+    data.l = 2;
+    data.A = new double* [data.l];
+    for (size_t i = 0; i < data.l; i++) {
+        data.A[i] = new double [data.k+1];
+        for (size_t j = 0; j < data.k+1; j++) {
+            data.A[i][j] = 1;
         }
     }
 }
 
-void Dispose_Data(INPUT_DATA & Data) {
-    if (Data.M) {
-        delete Data.M;
-        Data.M = 0;
+void DisposeData(INPUT_DATA & data) {
+    if (data.M) {
+        delete data.M;
+        data.M = 0;
     }
-    if (Data.A) {
-        for (size_t i = 0; i < Data.l; i++) {
-            if (Data.A[i]) {
-                delete Data.A[i];
-                Data.A[i] = 0;
+    if (data.A) {
+        for (size_t i = 0; i < data.l; i++) {
+            if (data.A[i]) {
+                delete data.A[i];
+                data.A[i] = 0;
             }
         }
-        delete Data.A;
-        Data.A = 0;
+        delete data.A;
+        data.A = 0;
+    }
+}
+
+void DisposeVector(double** ptrVector) {
+    if (*ptrVector) {
+        delete *ptrVector;
+        *ptrVector = 0;
+    }
+}
+
+void DisposeMatrix(const INPUT_DATA & data, double*** ptrMatrix) {
+    if (*ptrMatrix) {
+        for (size_t i = 0; i < data.N; i++) {
+            if ((*ptrMatrix)[i]) {
+                delete (*ptrMatrix)[i];
+                (*ptrMatrix)[i] = 0;
+            }
+        }
+        *ptrMatrix = 0;
+    }
+}
+
+void GetCurrentVector(const INPUT_DATA & data, Plant plant, double** ptrVector) {
+    if (*ptrVector == 0) {
+        *ptrVector = new double [data.k];
+    }
+    for (size_t i = 0; i < data.k; i++) {
+        (*ptrVector)[i] = plant_measure(data.M[i], plant);
+    }
+}
+
+void GetCalculatedVector(const INPUT_DATA & data, double* x, double** pZ) {
+    if (*pZ == 0) {
+        *pZ = new double [data.l];
+    }
+    for (size_t i = 0; i < data.l; i++) {
+        (*pZ)[i] = data.A[i][0];
+        for (size_t j = 0; j < data.k; j++) {
+            (*pZ)[i] += data.A[i][j+1] * x[j];
+        }
+    }
+}
+
+void WriteCalculatedVector(const INPUT_DATA & data, ofstream file, double* z) {
+    for (size_t i = 0; i < data.l; i++) {
+        cout << z[i] << "\t";
+        file << z[i] << endl;
+    }
+    cout << endl;
+}
+
+void ReadCalculatedVectors(const INPUT_DATA & data, ifstream file, double*** pZZ) {
+    *pZZ = new double* [data.N];
+    for (size_t i = 0; i < data.N; i++) {
+        (*pZZ)[i] = new double [data.l];
+        /*
+        for (size_t j = 0; j < data.l; j++) {
+            (*pZZ)[i][j] = 1; // ToDo
+        }
+        */
+        for (size_t j = 0; j < data.l; j++) {
+            file >> (*pZZ)[i][j];
+        }
+    }
+}
+
+double CalculateAverage(const INPUT_DATA & data, double* vector) {
+    double sum = 0;
+    for (size_t i = 0; i < data.N; i++) {
+        sum += vector[i];
+    }
+    return data.N ? sum/data.N : 0;
+}
+
+double CalculateVariance(const INPUT_DATA & data, double average, double* vector) {
+    double square_sum = 0;
+    for (size_t i = 0; i < data.N; i++) {
+        square_sum += vector[i] * vector[i];
+    }
+    if (data.N <= 1) {
+        return 0;
+    } else {
+        return (square_sum - data.N * average * average) / (data.N - 1);
     }
 }
