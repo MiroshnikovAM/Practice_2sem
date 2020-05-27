3ea4208b5805d9bc6d1efc5f495505d7e03d78be
MiroshnikovAM <kriogenika@yandex.ru>
2020-05-27 17:22:02 +0300

    code: research modified

--- main.cpp
+++ main.cpp
@@ -10,40 +10,38 @@ int main() {
     plant_init(plant);
     char* filename = "data.raw";
 
-    double* x = 0;
-    double* z = 0;
-    PrintHeader(params);
-    for (size_t i = 0; i < params.N; i++) {
-        GetCurrentVector(params, plant, &x);
-        GetCalculatedVector(params, x, &z);
-        PrintCurrentVector(params, i, x);
-        WriteCalculatedVector(params, filename, i, z);
-        Sleep(params.T);
-    }
-    DisposeVector(&x);
-    DisposeVector(&z);
-
-    double** zz = 0;
-    ReadCalculatedVectors(params, filename, &zz);
-    double average;
-    double variance;
-    // Main Part
-    /*
-    for (size_t i = 0; i < params.l; i++) {
-        average = CalculateAverage(params, zz[i]);
-        variance = CalculateVariance(params, average, zz[i]);
-        PrintFooter(params, average, variance, i);
-    }
-    */
-    // Research Part
     PrintResearchHeader();
-    size_t i = 0;
+    size_t step = 0;
+    double** zz = 0;
     for (double Aicur = params.Aimin; Aicur < params.Aimax; Aicur += params.dAi) {
+        double* x = 0;
+        double* z = 0;
+        // PrintHeader(params);
+        for (size_t i = 0; i < params.N; i++) {
+            GetCurrentVector(params, plant, &x);
+            GetCalculatedVector(params, x, &z);
+            // PrintCurrentVector(params, i, x);
+            WriteCalculatedVector(params, filename, i, z);
+            Sleep(params.T);
+        }
+        DisposeVector(&x);
+        DisposeVector(&z);
+
+        ReadCalculatedVectors(params, filename, &zz);
+        double average;
+        double variance;
+        /*
+        for (size_t i = 0; i < params.l; i++) {
+            average = CalculateAverage(params, zz[i]);
+            variance = CalculateVariance(params, average, zz[i]);
+            // PrintFooter(params, average, variance, i);
+        }
+        */
         params.A[0][params.researchCoeff] = Aicur;
-        average = CalculateAverage(params, zz[i]);
-        variance = CalculateVariance(params, average, zz[i]);
-        PrintResearchFooter(Aicur, variance, i + 1);
-        i++;
+        average = CalculateAverage(params, zz[0]);
+        variance = CalculateVariance(params, average, zz[0]);
+        PrintResearchFooter(Aicur, variance, step + 1);
+        step++;
     }
     DisposeMatrix(params, &zz);
     DisposeParams(params);
